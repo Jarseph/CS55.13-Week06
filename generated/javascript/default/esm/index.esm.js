de7f444ef92963cb9dd8c3781a9cfccef7eb1811
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2024-10-03 01:17:15 +0000

    Initial commit

---
+++ generated/javascript/default/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'CS5513-Week06',
+  location: 'us-central1'
+};
+
